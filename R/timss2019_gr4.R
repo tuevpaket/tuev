3cb1767eda4e749ccac500079f6330ceb42df7dd
tuevpaket <tuevpaket@gmail.com>
2021-05-28 22:31:42 +0300

    v1

---
+++ R/timss2019_gr4.R
@@ -0,0 +1,91 @@
+
+#' @title TIMSS 2019 Grade 4 Student-Teacher Linkage Data File of Turkey
+#'
+#' @description astturm7
+#'
+#' @format A \code{dataframe} with 8019 cases (rows) 105 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_astturm7"
+
+
+#' @title TIMSS 2019 Grade 4 Teacher Context Data File of Turkey
+#'
+#' @description atgturm7
+#'
+#' @format A \code{dataframe} with 360 cases (rows) 236 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#'}
+#'
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_atgturm7"
+
+
+#' @title TIMSS 2019 Grade 4 Within-country scoring reliability Data File of Turkey
+#'
+#' @description asrturm7
+#'
+#' @format A \code{dataframe} with 3119 cases (rows) 999 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_asrturm7"
+
+
+#' @title TIMSS 2019 Grade 4 Home context data file of Turkey
+#'
+#' @description ashturm7
+#'
+#' @format A \code{dataframe} with 4028 cases (rows) 109 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_ashturm7"
+
+
+#' @title TIMSS 2019 Grade 4 Student context data file of Turkey
+#'
+#' @description asgturm7
+#'
+#' @format A \code{dataframe} with 4028 cases (rows) 239 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_asgturm7"
+
+
+#' @title TIMSS 2019 Grade 4 Student achievement data file of Turkey
+#'
+#' @description asaturm7
+#'
+#' @format A \code{dataframe} with 4028 cases (rows) 1850 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_asaturm7"
+
+
+#' @title TIMSS 2019 Grade 4 School context data file of Turkey
+#'
+#' @description acgturm7
+#'
+#' @format A \code{dataframe} with 180 cases (rows) 102 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_acgturm7"
---
+++ R/timss2019_gr8.R
@@ -0,0 +1,181 @@
+#' @title TIMSS 2019 Grade 8 school context bridge data file of Turkey
+#'
+#' @description bcgturb7
+#'
+#' @format A \code{dataframe} with 72 cases (rows) 98 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_bcgturb7"
+
+
+#' @title TIMSS 2019 Grade 8 School context data file of Turkey
+#'
+#' @description bcgturm7
+#'
+#' @format A \code{dataframe} with 181 cases (rows) 98 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_bcgturm7"
+
+
+
+#' @title TIMSS 2019 Grade 8 student achievement bridge data file of Turkey
+#'
+#' @description bsaturb7
+#'
+#' @format A \code{dataframe} with 1819 cases (rows) 326 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_bsaturb7"
+
+
+#' @title TIMSS 2019 Grade 8 student achievement data file of Turkey
+#'
+#' @description bsaturm7
+#'
+#' @format A \code{dataframe} with 4077 cases (rows) 2073 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_bsaturm7"
+
+
+#' @title TIMSS 2019 Grade 8 student context bridge data file of Turkey
+#'
+#' @description bsgturb7
+#'
+#' @format A \code{dataframe} with 1819 cases (rows) 357 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_bsgturb7"
+
+
+#' @title TIMSS 2019 Grade 8 student context data file of Turkey
+#'
+#' @description bsgturm7
+#'
+#' @format A \code{dataframe} with 4077 cases (rows) 455 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_bsgturm7"
+
+
+#' @title TIMSS 2019 Grade 8 within-country scoring reliability bridge data file of Turkey
+#'
+#' @description bsrturb7
+#'
+#' @format A \code{dataframe} with 850 cases (rows) 324 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_bsrturb7"
+
+
+#' @title TIMSS 2019 Grade 8 within-country scoring reliability data file of Turkey
+#'
+#' @description bsrturm7
+#'
+#' @format A \code{dataframe} with 3649 cases (rows) 963 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_bsrturm7"
+
+
+#' @title TIMSS 2019 Grade 8 student-teacher linkage bridge file of Turkey
+#'
+#' @description bstturb7
+#'
+#' @format A \code{dataframe} with 3638 cases (rows) 45 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_bstturb7"
+
+
+#' @title TIMSS 2019 Grade 8 student-teacher linkage file of Turkey
+#'
+#' @description bstturm7
+#'
+#' @format A \code{dataframe} with 8154 cases (rows) 115 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_bstturm7"
+
+
+#' @title TIMSS 2019 Grade 8 mathematics teacher context bridge data file of Turkey
+#'
+#' @description btmturb7
+#'
+#' @format A \code{dataframe} with 72 cases (rows) 158 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_btmturb7"
+
+
+#' @title TIMSS 2019 Grade 8 mathematics teacher context data file of Turkey
+#'
+#' @description btmturm7
+#'
+#' @format A \code{dataframe} with 181 cases (rows) 158 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_btmturm7"
+
+
+#' @title TIMSS 2019 Grade 8 science teacher context bridge data file of Turkey
+#'
+#' @description btsturb7
+#'
+#' @format A \code{dataframe} with 72 cases (rows) 169 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_btsturb7"
+
+
+#' @title TIMSS 2019 Grade 8 science teacher context data file of Turkey
+#'
+#' @description btsturm7
+#'
+#' @format A \code{dataframe} with 181 cases (rows) 169 variables (columns).
+#' \describe{
+#' \item{ID...}{Identification variables}
+#' }
+#' @source \url{https://timss2019.org/international-database/}
+#' @references {Fishbein, B., Foy, P., & Yin, L. (2021). TIMSS 2019 User Guide for the International Database. Retrieved from Boston College, TIMSS & PIRLS International Study Center website: https://timssandpirls.bc.edu/timss2019/international-database/}
+"TIMSS19_btsturm7"
