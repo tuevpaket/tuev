45f8c0db19869c6ec8ccb31f617c7299a0ce8d7a
tuevpaket <tuevpaket@gmail.com>
2021-05-28 22:30:26 +0300

    v1

---
+++ data-raw/create_timms_data.R
@@ -0,0 +1,96 @@
+######### 2019 GR 4 #########
+TIMSS19_atgturm7 <- haven::read_sav("~/Documents/R package/timms/TIMSS-2019-Gr4/atgturm7.sav")
+usethis::use_data(TIMSS19_atgturm7, compress = "xz", overwrite = TRUE)
+
+
+TIMSS19_astturm7 <- haven::read_sav("~/Documents/R package/timms/TIMSS-2019-Gr4/astturm7.sav")
+usethis::use_data(TIMSS19_astturm7, compress = "xz", overwrite = TRUE)
+
+
+TIMSS19_asrturm7 <- haven::read_sav("~/Documents/R package/timms/TIMSS-2019-Gr4/asrturm7.sav")
+usethis::use_data(TIMSS19_asrturm7, compress = "xz", overwrite = TRUE)
+
+
+TIMSS19_ashturm7 <- haven::read_sav("~/Documents/R package/timms/TIMSS-2019-Gr4/ashturm7.sav")
+usethis::use_data(TIMSS19_ashturm7, compress = "xz", overwrite = TRUE)
+
+
+TIMSS19_asgturm7 <- haven::read_sav("~/Documents/R package/timms/TIMSS-2019-Gr4/asgturm7.sav")
+usethis::use_data(TIMSS19_asgturm7, compress = "xz", overwrite = TRUE)
+
+
+TIMSS19_asaturm7 <- haven::read_sav("~/Documents/R package/timms/TIMSS-2019-Gr4/asaturm7.sav")
+usethis::use_data(TIMSS19_asaturm7, compress = "xz", overwrite = TRUE)
+
+
+TIMSS19_acgturm7 <- haven::read_sav("~/Documents/R package/timms/TIMSS-2019-Gr4/acgturm7.sav")
+usethis::use_data(TIMSS19_acgturm7, compress = "xz", overwrite = TRUE)
+
+######### 2019 GR 8 #########
+library(haven)
+
+#Grade 8 school context bridge data files
+TIMSS19_bcgturb7 <- read_sav("~/Documents/R package/timms/TIMSS-2019-Gr8/bcgturb7.sav")
+usethis::use_data(TIMSS19_bcgturb7, compress = "xz", overwrite = TRUE)
+#Grade 8 school context data files
+TIMSS19_bcgturm7 <- read_sav("~/Documents/R package/timms/TIMSS-2019-Gr8/bcgturm7.sav")
+usethis::use_data(TIMSS19_bcgturm7, compress = "xz", overwrite = TRUE)
+
+
+#Grade 8 student achievement bridge data files
+TIMSS19_bsaturb7 <- read_sav("~/Documents/R package/timms/TIMSS-2019-Gr8/bsaturb7.sav")
+usethis::use_data(TIMSS19_bsaturb7, compress = "xz", overwrite = TRUE)
+#Grade 8 student achievement data files
+TIMSS19_bsaturm7 <- read_sav("~/Documents/R package/timms/TIMSS-2019-Gr8/bsaturm7.sav")
+usethis::use_data(TIMSS19_bsaturm7, compress = "xz", overwrite = TRUE)
+
+
+#Grade 8 student context bridge data files
+TIMSS19_bsgturb7 <- read_sav("~/Documents/R package/timms/TIMSS-2019-Gr8/bsgturb7.sav")
+usethis::use_data(TIMSS19_bsgturb7, compress = "xz", overwrite = TRUE)
+#Grade 8 student context data files
+TIMSS19_bsgturm7 <- read_sav("~/Documents/R package/timms/TIMSS-2019-Gr8/bsgturm7.sav")
+usethis::use_data(TIMSS19_bsgturm7, compress = "xz", overwrite = TRUE)
+
+
+#Grade 8 within-country scoring reliability bridge data files
+TIMSS19_bsrturb7 <- read_sav("~/Documents/R package/timms/TIMSS-2019-Gr8/bsrturb7.sav")
+usethis::use_data(TIMSS19_bsrturb7, compress = "xz", overwrite = TRUE)
+#Grade 8 within-country scoring reliability data files
+TIMSS19_bsrturm7 <- read_sav("~/Documents/R package/timms/TIMSS-2019-Gr8/bsrturm7.sav")
+usethis::use_data(TIMSS19_bsrturm7, compress = "xz", overwrite = TRUE)
+
+
+#Grade 8 student-teacher linkage bridge files
+TIMSS19_bstturb7 <- read_sav("~/Documents/R package/timms/TIMSS-2019-Gr8/bstturb7.sav")
+usethis::use_data(TIMSS19_bstturb7, compress = "xz", overwrite = TRUE)
+#Grade 8 student-teacher linkage files
+TIMSS19_bstturm7 <- read_sav("~/Documents/R package/timms/TIMSS-2019-Gr8/bstturm7.sav")
+usethis::use_data(TIMSS19_bstturm7, compress = "xz", overwrite = TRUE)
+
+
+#Grade 8 mathematics teacher context bridge data files
+TIMSS19_btmturb7 <- read_sav("~/Documents/R package/timms/TIMSS-2019-Gr8/btmturb7.sav")
+usethis::use_data(TIMSS19_btmturb7, compress = "xz", overwrite = TRUE)
+#Grade 8 mathematics teacher context data files
+TIMSS19_btmturm7 <- read_sav("~/Documents/R package/timms/TIMSS-2019-Gr8/btmturm7.sav")
+usethis::use_data(TIMSS19_btmturm7, compress = "xz", overwrite = TRUE)
+
+
+#Grade 8 science teacher context bridge data files
+TIMSS19_btsturb7 <- read_sav("~/Documents/R package/timms/TIMSS-2019-Gr8/btsturb7.sav")
+usethis::use_data(TIMSS19_btsturb7, compress = "xz", overwrite = TRUE)
+#Grade 8 science teacher context data files
+TIMSS19_btsturm7 <- read_sav("~/Documents/R package/timms/TIMSS-2019-Gr8/btsturm7.sav")
+usethis::use_data(TIMSS19_btsturm7, compress = "xz", overwrite = TRUE)
+
+
+
+######### 2019 GR 4 #########
+
+#usethis::use_data(, compress = "xz", overwrite = TRUE)
+
+
+usethis::use_data(PISA_COG_2018, compress = "xz", overwrite = TRUE)
+usethis::use_data(PISA_OGR_2018, compress = "xz", overwrite = TRUE)
+
